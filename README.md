49bbef8b9fe41971534010ca04e4e11a1f08d247
1chooo <hugo970217@gmail.com>
2023-11-20 22:59:34 +0800

    init: lambda line bot

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# saving-bot
+# Saving Tracker
+
+
+## Create `line-bot-sdk` Layer [^1]
+
+```shell
+$ mkdir python
+$ cd python
+$ pip install --target . line-bot-sdk 
+$ cd ..
+$ zip -r line-bot-sdk.zip ./python
+$ rm -rf ./python
+```
+
+### With shell script
+```shell
+$ ./build_line_bot_sdk_layer.sh
+```
+
+## Add Permission with s3 [^2]
+```js
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::your_bucket_name/*"
+            ]
+        }
+    ]
+}
+```
+
+## CONTACT INFO.
+
+> AWS Educate Cloud Ambassador, Technical Support </br>
+> **Hugo ChunHo Lin**
+> 
+> <aside>
+>   📩 E-mail: <a href="mailto:hugo970217@gmail.com">hugo970217@gmail.com</a>
+> <br>
+>   🧳 Linkedin: <a href="https://www.linkedin.com/in/1chooo/">Hugo ChunHo Lin</a>
+> <br>
+>   👨🏻‍💻 GitHub: <a href="https://github.com/1chooo">1chooo</a>
+>    
+> </aside>
+
+## License
+Released under [MIT](./LICENSE) by [AWS Educate TW](https://aws.amazon.com/tw/education/awseducate/), [Hugo ChunHo Lin](https://github.com/1chooo).
+
+This software can be modified and reused without restriction.
+The original license must be included with any copies of this software.
+If a significant portion of the source code is used, please provide a link back to this repository.
+
+[^1]: [使用 .zip 封存檔部署 Python Lambda 函數](https://docs.aws.amazon.com/zh_tw/lambda/latest/dg/python-package.html)
+[^2]: [Policies and Permissions in Amazon S3](https://docs.aws.amazon.com/AmazonS3/latest/userguide/access-policy-language-overview.html?icmpid=docs_amazons3_console)
---
+++ lambda_function.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Date: 2023/11/20
+Author: @1chooo(Hugo ChunHo Lin)
+E-mail: hugo970217@gmail.com
+Version: v0.1.0
+"""
+
+import os
+import json
+from linebot import LineBotApi
+from linebot import WebhookHandler
+from linebot.models import MessageEvent
+from linebot.models import TextMessage
+from linebot.models import ImageMessage
+from linebot.models import TextSendMessage
+from linebot.models import ImageSendMessage
+from linebot.models import VideoSendMessage
+from linebot.exceptions import InvalidSignatureError
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+def lambda_handler(event, context):
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_text_message(event):
+
+        event_text = event.message.text
+
+        if event_text == "Hello":
+            reply_messages = [
+                TextSendMessage(
+                    text=f'World'
+                ),
+            ]
+                
+            line_bot_api.reply_message(
+                event.reply_token,
+                reply_messages
+            )
+        elif event_text == "我想看帥哥":
+            reply_messages = [
+                TextSendMessage(
+                    text=f'Test get image from s3 public bucket'
+                ),
+                TextSendMessage(
+                    text=f'This is Hugo!'
+                ),
+                ImageSendMessage(
+                    original_content_url = "https://2023-amazon-ambassador.s3.amazonaws.com/hugo_grad.png",
+                    preview_image_url = "https://2023-amazon-ambassador.s3.amazonaws.com/hugo_grad.png",
+                ),
+            ]
+                
+            line_bot_api.reply_message(
+                event.reply_token,
+                reply_messages
+            )
+        elif event_text == "Image":
+            reply_messages = [
+                TextSendMessage(
+                    text=f'Test get image from s3 public bucket'
+                ),
+                TextSendMessage(
+                    text=f'This is Hugo!'
+                ),
+                ImageSendMessage(
+                    original_content_url = "https://2023-amazon-ambassador.s3.amazonaws.com/hugo_grad.png",
+                    preview_image_url = "https://2023-amazon-ambassador.s3.amazonaws.com/hugo_grad.png",
+                ),
+            ]
+                
+            line_bot_api.reply_message(
+                event.reply_token,
+                reply_messages
+            )
+        else:
+            reply_messages = [
+                TextSendMessage(
+                    text=f'{event_text}'
+                ),
+            ]
+            line_bot_api.reply_message(
+                event.reply_token,
+                reply_messages
+            )
+
+
+    try:
+        # get X-Line-Signature header value
+        signature = event['headers']['x-line-signature']
+
+        # get request body as text
+        body = event['body']
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return {
+            'statusCode': 502,
+            'body': json.dumps("Invalid signature. Please check your channel access token/channel secret.")
+        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Hello from Lambda!")
+    }
